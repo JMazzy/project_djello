72682976b238f6e03236322e58a2b4ef6f91618b
Joshua Masland-Sarani <j.masland@gmail.com>
2016-04-05 17:02:58 -0400

    added ordering by filter

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,9 +4,9 @@ class StaticPagesController < ApplicationController
 
   def index
     if user_signed_in?
-      
+
     else
-      redirect_to new_user_session
+      redirect_to new_user_session_path
     end
   end
 
